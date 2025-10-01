9a787ed2da6758f4349559f454ec71c9c908aa88
aescalonac <aescalonac@iesrafaelalberti.es>
2025-10-01 19:20:21 +0200

    Ejercicio 1.2.2 funciona

--- 1º TRIMESTRE/UNIDAD 1/Ejercicios 1.2/ej1.2.2.py
+++ 1º TRIMESTRE/UNIDAD 1/Ejercicios 1.2/ej1.2.2.py
@@ -1,4 +1,5 @@
 #Escribe un programa para pedirle al usuario las horas de trabajo y el precio por hora y calcule el importe total del servicio. 
-horas = int(input ("Escribe tus horas de trabajo: ")) 
-precio = int(input ("Escribe el precio por hora de trabajo: "))
-m =  
+horas = float(input ("Escribe tus horas de trabajo: ")) 
+precio = float(input ("Escribe el precio por hora de trabajo: "))
+m = horas*precio
+print ("El importe total del servicio realizado es: ", m , "euros")
