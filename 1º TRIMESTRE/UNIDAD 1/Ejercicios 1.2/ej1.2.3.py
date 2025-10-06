2cd90fefa6bc236b6e3f79e9dabcf435bc8a9021
aescalonac <aescalonac@iesrafaelalberti.es>
2025-10-06 19:29:25 +0200

    Ejercicio 3 Fuunciona

---
+++ 1º TRIMESTRE/UNIDAD 1/Ejercicios 1.2/ej1.2.3.py
@@ -0,0 +1,12 @@
+ancho = 17
+alto =  12.0
+
+a = ancho / 2
+b = ancho // 2
+c = alto / 3
+d = 1 + 2 * 5
+
+print ("El apartado 1. da ", a)
+print ("El apartado 2. da ", b)
+print ("El apartado 3. da ", c)
+print ("El apartado 4. da ", d)
