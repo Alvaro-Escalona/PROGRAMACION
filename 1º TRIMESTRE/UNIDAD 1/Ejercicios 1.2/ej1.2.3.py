b2521900d2a7d6420563e62b6fb44786698f4317
aescalonac <aescalonac@iesrafaelalberti.es>
2025-10-06 19:50:37 +0200

    Ejercicio 3 no funciona

--- 1º TRIMESTRE/UNIDAD 1/Ejercicios 1.2/ej1.2.3.py
+++ 1º TRIMESTRE/UNIDAD 1/Ejercicios 1.2/ej1.2.3.py
@@ -6,7 +6,7 @@ b = ancho // 2
 c = alto / 3
 d = 1 + 2 * 5
 
-print ("El apartado 1. da ", a)
-print ("El apartado 2. da ", b)
-print ("El apartado 3. da ", c)
-print ("El apartado 4. da ", d)
+print ("El apartado 1. da ", 4)
+print ("El apartado 2. da ", 2)
+print ("El apartado 3. da ", 9)
+print ("El apartado 4. da ", 3)
