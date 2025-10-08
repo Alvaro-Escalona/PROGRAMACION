7cbb022510aa9ce4f59bd49bcb48b712eddbe5af
aescalonac <aescalonac@iesrafaelalberti.es>
2025-10-08 15:46:02 +0200

    Ejercicio1.2.7 FUNCIONA

---
+++ 1º TRIMESTRE/UNIDAD 1/Ejercicios 1.2/ej1.2.7.py
@@ -0,0 +1,6 @@
+#Escribe un programa que solicite tres números al usuario y calcule e imprima por pantalla su suma.
+n1 = int(input("Introduce el primer número: "))
+n2 = int(input("Introduce el segundo número: "))
+n3 = int(input("Introduce el tercer número: "))
+suma = n1 + n2 + n3
+print ("La suma de", n1,"+",n2,"+",n3,"es igual a", suma)
---
+++ 1º TRIMESTRE/UNIDAD 1/Ejercicios 1.2/ej1.2.8.py
@@ -0,0 +1,1 @@
+#Escribir el programa del ejercicio 1.7 usando solamente dos variables diferentes. 
