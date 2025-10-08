9a816f0e086d0a8c9ae3113778689280d6220bc4
aescalonac <aescalonac@iesrafaelalberti.es>
2025-10-08 16:29:03 +0200

    Ejercicio1.2.8 FUNCIONA

--- 1º TRIMESTRE/UNIDAD 1/Ejercicios 1.2/ej1.2.7.py
+++ 1º TRIMESTRE/UNIDAD 1/Ejercicios 1.2/ej1.2.7.py
@@ -2,5 +2,7 @@
 n1 = int(input("Introduce el primer número: "))
 n2 = int(input("Introduce el segundo número: "))
 n3 = int(input("Introduce el tercer número: "))
+
 suma = n1 + n2 + n3
+
 print ("La suma de", n1,"+",n2,"+",n3,"es igual a", suma)
---
+++ 1º TRIMESTRE/UNIDAD 1/Ejercicios 1.2/ej1.2.8.1.py
@@ -0,0 +1,5 @@
+n = 0
+for i in range (3):
+    n = int(input("Introduce un numero: "))
+    n += n + n 
+    print (n)
